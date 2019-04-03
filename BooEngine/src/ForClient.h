feef526018d0887f12a4335ef62f08eabe3d0807
Concarster <concarster01@gmail.com>
2019-04-03 18:48:51 -0400

    Setup & Window

---
+++ BooEngine/src/ForClient.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/* Clients Only */
+#include "Engine\Util\Log.h"
+#include "Engine\Engine.h"
+#include "Engine\Layers\Layer.h"
+#include "Engine\InputMgr.h"
+
+#include "Engine\Events\KeyEvent.h"
